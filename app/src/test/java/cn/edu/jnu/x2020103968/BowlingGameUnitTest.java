0f0353506f5ed099a22bee70e54cc260f2b1b132
Wuheng <2549108672@qq.com>
2023-04-10 21:08:18 +0800

    第二次 commit

--- app/src/test/java/cn/edu/jnu/x2020103968/BowlingGameUnitTest.java
+++ app/src/test/java/cn/edu/jnu/x2020103968/BowlingGameUnitTest.java
@@ -13,6 +13,11 @@ public class BowlingGameUnitTest {
     @Test
     public void test20Zeros()
     {
-
+        BowlingGame game = new BowlingGame();
+        for(int i=0;i<20;i++)
+        {
+            game.roll(0);
+        }
+        assertEquals(0,game.score());
     }
 }
