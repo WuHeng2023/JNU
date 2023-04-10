201b7fe711a9fdcaf1795c1d734ca856c0969953
Wuheng <2549108672@qq.com>
2023-04-10 21:42:04 +0800

    第五次 commit

--- app/src/main/java/cn/edu/jnu/x2020103968/BowlingGame.java
+++ app/src/main/java/cn/edu/jnu/x2020103968/BowlingGame.java
@@ -1,10 +1,25 @@
 package cn.edu.jnu.x2020103968;
 
 public class BowlingGame {
+    int score=0;
+//    int pins[] =new int[21];
+//    int current_pin_index=0;
     public void roll(int pin) {
+        score+=pin;
     }
 
     public int score() {
-        return 0;
+//        int totalScore=0;
+//        for(int scoreIndex=0;scoreIndex<pins.length;scoreIndex++)
+//        {
+//            totalScore+=pins[scoreIndex];
+//            //如果是补中，就把下下球分数计入
+//            if(scoreIndex<19){
+//                if(10==pins[scoreIndex]+pins[scoreIndex+1]) {
+//                    totalScore+=pins[scoreIndex+2];
+//                }
+//            }
+//        }
+        return score;
     }
 }
